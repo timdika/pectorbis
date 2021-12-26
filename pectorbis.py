48b29164169320adc1d08d9a4d58182ff5ed1504
Timofej Kazakov vorher Scott Future <timofej.kazakov.privat@gmail.com>
2021-12-26 21:05:50 +0100

    Python Datei init - pectorbis.py

---
+++ pectorbis.py
@@ -0,0 +1,10 @@
+#Pectorbis von Timofej D. Kazakov (Github: timdika)
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+labels = os.listdir("")
+
+#Kopie von ganzer Datei und das dann bearbeiten
